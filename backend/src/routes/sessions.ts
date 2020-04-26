949a43d611d935a5c34d902a3b71e5786ba5d8a8
lucasbalduino <lucas.balduino@nexenergy.com.br>
2020-04-26 20:59:40 -0300

    credentials validity

---
+++ backend/src/routes/sessions.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUserService = new AuthenticateUserService();
+
+    const { user } = await authenticateUserService.execute({
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return response.json({ user });
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export default sessionsRouter;
