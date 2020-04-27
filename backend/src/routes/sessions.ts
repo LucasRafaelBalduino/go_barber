581d227a9910954af3d4dfda2010481ed5f38901
lucasbalduino <lucas.balduino@nexenergy.com.br>
2020-04-26 21:11:23 -0300

    generating jwt token

--- backend/src/routes/sessions.ts
+++ backend/src/routes/sessions.ts
@@ -10,14 +10,14 @@ sessionsRouter.post('/', async (request, response) => {
 
     const authenticateUserService = new AuthenticateUserService();
 
-    const { user } = await authenticateUserService.execute({
+    const { user, token } = await authenticateUserService.execute({
       email,
       password,
     });
 
     delete user.password;
 
-    return response.json({ user });
+    return response.json({ user, token });
   } catch (error) {
     return response.status(400).json({ error: error.message });
   }
--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,12 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/ password combination');
     }
 
-    return { user };
+    const token = sign({}, 'cc5b7b468c8a4b8d551a2cf70006ac8f', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
