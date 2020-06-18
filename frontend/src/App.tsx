a4bbae906bf6ce84521706df2eacf58dce18fcfd
Balduino <lukasrafael1220@gmail.com>
2020-06-18 06:01:49 -0300

    :tada: configuring structure and styles global

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello</h1>
+    <GlobalStyle />
+  </>
+);
+export default App;
