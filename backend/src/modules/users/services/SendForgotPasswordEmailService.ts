693d3482b5a5eb4bda9bb9d7c96cad16092ae448
Balduino <lukasrafael1220@gmail.com>
2020-07-04 16:54:54 -0300

    :white_check_mark: recover and reset password tests

---
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppErro';
+// import User from '@modules/users/infra/typeorm/entities/User';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokenRepository from '../repositories/IUserTokenRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokenRepository')
+    private userTokenRepository: IUserTokenRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does note exists.');
+    }
+
+    await this.userTokenRepository.generate(user.id);
+
+    this.mailProvider.sendMail(email, 'fazendo teste de forgout');
+  }
+}
+
+export default SendForgotPasswordEmailService;
